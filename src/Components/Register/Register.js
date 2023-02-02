5394f3720cb0027938ad73ab80bd9903cdafb96a
Mohamed Mbarek <72167568+MohamedMbaarek@users.noreply.github.com>
2023-02-03 00:31:45 +0100

    register instead of Register

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitRegister = () => {
-        fetch('https://smart-brain-api-xzci.onrender.com/Register', {
+        fetch('https://smart-brain-api-xzci.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
